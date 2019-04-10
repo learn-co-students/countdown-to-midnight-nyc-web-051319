695e0bfeb730d19fb3996aad16b0fc41ad1ed633
Elma Rahman <elmarahman635@gmail.com>
2019-04-10 22:17:08 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+
+  loop do
+    if integer > 0
+   puts "#{integer} SECOND(S)!"
+    integer -= 1
+  else
+    return "HAPPY NEW YEAR!"
+
+  end
+
+
+end
+end
+
+def countdown_with_sleep (seconds_to_midnight)
+  sleep 5  
+
 end
