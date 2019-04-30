f0dab9bec564868f72fc0eecd3bdd4c70e43dead
William Bainton <baintonw@gmail.com>
2019-04-30 20:59:47 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
-
-def countdown
+def countdown(counter)
+  
+  while counter > 0
+    puts "#{counter} SECOND(S) !"
+    counter -= 1
+  end
+  
+  if counter == 0
+    return "HAPPY NEW YEAR!"
+    
+    
+  end
 end
